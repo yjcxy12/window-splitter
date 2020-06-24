3b5f0c825bcadb03908a278c1c0c34154d0cf618
Xiangyun Chi <xiangyun.chi@tote.co.uk>
2020-06-24 22:05:35 +0100

    fix: update hook dependency

--- hooks/useChainHandlerProp.js
+++ hooks/useChainHandlerProp.js
@@ -1,8 +1,11 @@
 import { useCallback } from 'react';
 
 export default (handler, handlerProp) => {
-  return useCallback(e => {
-    if (handler) handler(e);
-    if (handlerProp) handlerProp(e);
-  });
+  return useCallback(
+    e => {
+      if (handler) handler(e);
+      if (handlerProp) handlerProp(e);
+    },
+    [handler, handlerProp]
+  );
 };
